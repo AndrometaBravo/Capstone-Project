aa04722201f2a565b1dc25a6508c9ebc4230cab2
Travis Baker <travi5jab@Traviss-MacBook-Pro.local>
2019-06-18 09:35:25 -0700

    database changes

--- capstone/test/models/user_test.rb
+++ capstone/test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "should not sign up" do
+        user = User.new(email:"fake@email.com")
+        pp user
+        assert_not user.save
+    end
+
 end
