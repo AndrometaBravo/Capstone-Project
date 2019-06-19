8d4023a092e1072399c799d3498756d7ad807bfa
swhite21 <sebastian.swhite@gmail.com>
2019-06-19 15:49:45 -0700

    refined homepage layout. add dummy post section and filter to home.

--- capstone/app/javascript/components/feedTopNav.js
+++ capstone/app/javascript/components/feedTopNav.js
@@ -2,8 +2,12 @@ import React from "react"
 import PropTypes from "prop-types"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faCog, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { DropdownMenu, DropdownToggle, UncontrolledDropdown, DropdownItem } from 'reactstrap'
 
 class FeedTopNav extends React.Component {
+  gearClick=()=>{
+    console.log("gearClickran");
+  }
   render () {
     return (
       <div className="feed-top-nav">
@@ -12,7 +16,33 @@ class FeedTopNav extends React.Component {
         <img className="feed-avatar-small" src="https://i0.wp.com/zblogged.com/wp-content/uploads/2019/02/FakeDP.jpeg?resize=567%2C580&ssl=1" alt="Card image cap" />
       </div>
       <div id="feed-username">User Name</div>
-          <FontAwesomeIcon icon={faCog} color="grey" size="lg" pull="right"/>
+      <div id="cog-dropdown">
+      <UncontrolledDropdown>
+            <DropdownToggle caret>
+              <FontAwesomeIcon icon={faCog} color="white" size="lg" pull="right"/>
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>
+                  <a
+                     href="/">
+                     Home
+                  </a>
+              </DropdownItem>
+              <DropdownItem>
+                  <a
+                     href="/about">
+                     About
+                  </a>
+              </DropdownItem>
+              <DropdownItem>
+                  <a
+                     href="/more">
+                     More
+                  </a>
+              </DropdownItem>
+            </DropdownMenu>
+       </UncontrolledDropdown>
+       </div>
       </div>
        );
      }
--- capstone/app/javascript/components/home.js
+++ capstone/app/javascript/components/home.js
@@ -8,41 +8,14 @@ import { faCoffee, faCog } from '@fortawesome/free-solid-svg-icons'
 import{Card, CardImg, Button, CardTitle, CardBody, CardSubtitle, CardText} from 'reactstrap'
 import MapContainer from './mapcontainer'
 import CloudFeed from  './feed'
+import NewPostBox from './newPostBox'
 
 
 class Home extends React.Component {
   constructor(props){
     super(props)
     this.state={
-      feed:[{
-            post:"this is just fake text. how long should a post be 250 characters?",
-            userName: "John Something",
-            currentStatus: "Available",
-            profilePic: "https://www.evolutionsociety.org/userdata/news_picupload/pic_sid189-0-norm.jpg",
-            lat: 32.708910,
-            lng:-117.159447
-            },
-            {
-            post:"this is just fake text. how long should a post be 250 characters?",
-            userName: "John Something",
-            currentStatus: "Available",
-            profilePic: "https://www.evolutionsociety.org/userdata/news_picupload/pic_sid189-0-norm.jpg",
-            lat: 32.709331,
-            lng:-117.159827
-            },
-            {
-            post:"this is just fake text. how long should a post be 250 characters?",
-            userName: "John Something",
-            currentStatus: "Available",
-            profilePic: "https://www.evolutionsociety.org/userdata/news_picupload/pic_sid189-0-norm.jpg",
-            lat: 32.708910,
-            lng:-117.159447
-            }
-      ],
-      myLocation:{
-        lat: 32.709144,
-        lng:-117.159194
-      }
+
     }
   }
   componentDIdMount=()=>{
@@ -56,15 +29,40 @@ class Home extends React.Component {
     let{ renderProfiles }=this
     let{ posts }=this.props
     return (
-        <div id="map-holder">
-          <div id="fake-feed">
+        <div className="grid-container">
+          <div className="Feed">
           <FeedTopNav />
-          <CloudFeed posts={posts}/>
+            <div className="Feed-Posts">
+              <CloudFeed posts={posts}/>
+            </div>
           </div>
+          <div className="Map-Container">
           <MapContainer
             myLocation={myLocation}
             feed={feed}
           />
+          <div className="Comment-Box">
+            <p>Compose New Post</p>
+            <Button>Submit</Button>
+          </div>
+          <div className="Filter-Area">
+          <p>Filter</p>
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Cras
+              <span className="badge badge-primary badge-pill">14</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Dapibus ac facilisis in
+              <span className="badge badge-primary badge-pill">2</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Morbi leo risus
+              <span className="badge badge-primary badge-pill">1</span>
+            </li>
+            </ul>
+          </div>
+          </div>
         </div>
 
        );
